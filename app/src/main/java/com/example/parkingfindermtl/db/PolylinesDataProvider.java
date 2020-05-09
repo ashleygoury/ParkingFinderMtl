090a62dc1487eda30872e862c9eb15fb83ac2df5
ashley <ashleygoury@outlook.com>
2020-05-08 20:32:12 -0400

    read and display polyline from realm

--- app/src/main/java/com/example/parkingfindermtl/db/PolylinesDataProvider.java
+++ app/src/main/java/com/example/parkingfindermtl/db/PolylinesDataProvider.java
@@ -16,17 +16,25 @@ public class PolylinesDataProvider {
     static {
         parkingList = new ArrayList<>();
 
-        addPolyline(new Polyline(true, 45.547944
-                        , -73.571238
-                        , 45.548626
-                        , -73.570532
+        addPolyline(new Polyline(true, 45.736301
+                        , -73.824717
+                        , 45.742785
+                        , -73.826616
+                        , "Rosemont", "La Petite-Patrie"),
+                new Day(1,
+                        new Time(7, 8, 30, 30)));
+
+        addPolyline(new Polyline(true, 45.743331
+                        , -73.826693
+                        , 45.743331
+                        , -73.825924
                         , "Rosemont", "La Petite-Patrie"),
                 new Day(1,
                         new Time(7, 8, 30, 30)));
     }
 
     private static void addPolyline(Polyline polyline, Day... days) {
-        if(days.length > 0 ) {
+        if (days.length > 0) {
             polyline.setDays((new RealmList<Day>()));
             polyline.getDays().addAll(Arrays.asList(days));
         }
